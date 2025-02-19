0828eda8143c2a111f0fb47447f87ee841e914c8
SulaymonGit <buuusuuu7@gmail.com>
2025-02-19 22:12:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let aftor = [2, 4, 9, 10, 45, 67, 8, 90];
+console.log(aftor, 'boshlanishdagi array');
+
+
+let before = aftor.filter(num => num % 2 === 0);
+console.log(before, 'juft sonlarga ajratilgan array');
